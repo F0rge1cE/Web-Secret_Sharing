1a9e9272eefe23ebedfbfcd274314d87b50eee1d
Neptunennn <guoyuanwu@gatech.edu>
2018-04-25 17:56:20 -0400

    webframe

--- Web_server/secret_sharing.py
+++ Web_server/secret_sharing.py
@@ -101,36 +101,80 @@ class MainPage(webapp2.RequestHandler):
         self.response.write(template.render(template_values))
 # [END main_page]
 
+class Encrypt(webapp2.RequestHandler):
+    def get(self):
+        # guestbook_name = self.request.get('guestbook_name',
+        #                                   DEFAULT_GUESTBOOK_NAME)
+        # greetings_query = Greeting.query(
+        #     ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
+        # greetings = greetings_query.fetch(10)
+        user = users.get_current_user()
+        if user:
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
+
+        template_values = {
+            'user': user,
+            'url': url,
+            'url_linktext': url_linktext,
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('./template/encrypt.html')
+        self.response.write(template.render(template_values))
+
 
 # [START guestbook]
-class Guestbook(webapp2.RequestHandler):
 
-    def post(self):
-        # We set the same parent key on the 'Greeting' to ensure each
-        # Greeting is in the same entity group. Queries across the
-        # single entity group will be consistent. However, the write
-        # rate to a single entity group should be limited to
-        # ~1/second.
-        guestbook_name = self.request.get('guestbook_name',
-                                          DEFAULT_GUESTBOOK_NAME)
-        greeting = Greeting(parent=guestbook_key(guestbook_name))
-
-        if users.get_current_user():
-            greeting.author = Author(
-                    identity=users.get_current_user().user_id(),
-                    email=users.get_current_user().email())
-
-        greeting.content = self.request.get('content')
-        greeting.put()
-
-        query_params = {'guestbook_name': guestbook_name}
-        self.redirect('/?' + urllib.urlencode(query_params))
 # [END guestbook]
+class Decrypt(webapp2.RequestHandler):
+
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
+
+        template_values = {
+            'user': user,
+            'url': url,
+            'url_linktext': url_linktext,
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('./template/decrypt.html')
+        self.response.write(template.render(template_values))
+
+class Success(webapp2.RequestHandler):
+
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
+
+        template_values = {
+            'user': user,
+            'url': url,
+            'url_linktext': url_linktext,
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('./template/success.html')
+        self.response.write(template.render(template_values))
 
 
 # [START app]
 app = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/sign', Guestbook),
+    ('/encrypt', Encrypt),
+    ('/decrypt', Decrypt),
+    ('/success', Success)
 ], debug=True)
 # [END app]
