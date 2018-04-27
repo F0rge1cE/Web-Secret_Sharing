e72878bb75cd6844215b9972744d340a94a9a8e2
Neptunennn <guoyuanwu@gatech.edu>
2018-04-26 21:07:25 -0400

    gmailAPI

--- Web_server/secret_sharing.py
+++ Web_server/secret_sharing.py
@@ -21,8 +21,25 @@ import urllib
 from google.appengine.api import users
 from google.appengine.ext import ndb
 
+import httplib2
+import os
+import oauth2client
+from oauth2client import client, tools
+import base64
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from apiclient import errors, discovery
+import mimetypes
+from email.mime.image import MIMEImage
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+
 import jinja2
 import webapp2
+
+SCOPES = 'https://www.googleapis.com/auth/gmail.send'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Gmail API Python Send Email'
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -30,27 +47,54 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     autoescape=True)
 # [END imports]
 
-DEFAULT_GUESTBOOK_NAME = 'default_guestbook'
+def get_credentials():
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'gmail-python-email-send.json')
+    store = oauth2client.file.Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        credentials = tools.run_flow(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+def SendMessage(sender, to, subject, msgHtml, msgPlain, attachmentFile=None):
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('gmail', 'v1', http=http)
+    if attachmentFile:
+        message1 = createMessageWithAttachment(sender, to, subject, msgHtml, msgPlain, attachmentFile)
+    else: 
+        message1 = CreateMessageHtml(sender, to, subject, msgHtml, msgPlain)
+    result = SendMessageInternal(service, "me", message1)
+    return result
+
+def SendMessageInternal(service, user_id, message):
+    try:
+        message = (service.users().messages().send(userId=user_id, body=message).execute())
+        print('Message Id: %s' % message['id'])
+        return message
+    except errors.HttpError as error:
+        print('An error occurred: %s' % error)
+        return "Error"
+    return "OK"
+
+def CreateMessageHtml(sender, to, subject, msgHtml, msgPlain):
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = to
+    msg.attach(MIMEText(msgPlain, 'plain'))
+    msg.attach(MIMEText(msgHtml, 'html'))
+    return {'raw': base64.urlsafe_b64encode(msg.as_string())}
 
 
-# We set a parent key on the 'Greetings' to ensure that they are all
-# in the same entity group. Queries across the single entity group
-# will be consistent. However, the write rate should be limited to
-# ~1/second.
 
-def guestbook_key(guestbook_name=DEFAULT_GUESTBOOK_NAME):
-    """Constructs a Datastore key for a Guestbook entity.
-
-    We use guestbook_name as the key.
-    """
-    return ndb.Key('Guestbook', guestbook_name)
-
-
-# [START greeting]
-class Author(ndb.Model):
-    """Sub model for representing an author."""
-    identity = ndb.StringProperty(indexed=False)
-    email = ndb.StringProperty(indexed=False)
 
 
 class File(ndb.Model):
@@ -62,13 +106,6 @@ class File(ndb.Model):
 
 
 
-
-class Greeting(ndb.Model):
-    """A main model for representing an individual Guestbook entry."""
-    author = ndb.StructuredProperty(Author)
-    content = ndb.StringProperty(indexed=False)
-    date = ndb.DateTimeProperty(auto_now_add=True)
-# [END greeting]
 
 
 # [START main_page]
@@ -95,7 +132,7 @@ class MainPage(webapp2.RequestHandler):
 
 class Encrypt(webapp2.RequestHandler):
     def get(self):
-
+        except_ = self.request.get('except', False)
         user = users.get_current_user()
         if user:
             url = users.create_logout_url(self.request.uri)
@@ -108,23 +145,44 @@ class Encrypt(webapp2.RequestHandler):
             'user': user,
             'url': url,
             'url_linktext': url_linktext,
+            'except' : except_
         }
 
         template = JINJA_ENVIRONMENT.get_template('./template/encrypt.html')
         self.response.write(template.render(template_values))
 
     def post(self):
+        try:
+            file = self.request.POST.get('raw_file').file.read()
+        except:
+            query_params = {
+                'except' : True
+            }
+            self.redirect('/encrypt?' + urllib.urlencode(query_params))
+        else:
+            name = self.request.get('name')
+            N_share = self.request.get('N_share')
+            K_require = self.request.get('K_require')
+            print("****************************************")
+            print(file)
+            print("add encrypt algorithm here")
+            print("****************************************")
 
-        file = self.request.POST.get('raw_file').file.read()
 
-        print("****************************************")
-        print(file)
-        print("add encrypt algorithm here")
-        print("****************************************")
-        query_params = {
-            'encrypt': True,
-        }
-        self.redirect('/success?' + urllib.urlencode(query_params))
+            #add gmail api here
+            to = "xuxy1994@gmail.com"
+            sender = "guoyuanwu666@gmail.com"
+            subject = "subject"
+            msgHtml = file
+            msgPlain = "Hi\nPlain Email"
+            SendMessage(sender, to, subject, msgHtml, msgPlain)
+
+
+
+            query_params = {
+                'encrypt': True,
+            }
+            self.redirect('/success?' + urllib.urlencode(query_params))
 
 
 # [START guestbook]
@@ -133,6 +191,7 @@ class Encrypt(webapp2.RequestHandler):
 class Decrypt(webapp2.RequestHandler):
 
     def get(self):
+        except_ = self.request.get('except', False)
         user = users.get_current_user()
         if user:
             url = users.create_logout_url(self.request.uri)
@@ -145,23 +204,32 @@ class Decrypt(webapp2.RequestHandler):
             'user': user,
             'url': url,
             'url_linktext': url_linktext,
+            'except' : except_
         }
 
         template = JINJA_ENVIRONMENT.get_template('./template/decrypt.html')
         self.response.write(template.render(template_values))
 
     def post(self):
-        file = self.request.POST.getall('raw_file')
-        for f in file:
-            sub_file = f.file.read()
-            print("****************")
-            print(sub_file)
-        # add reconstruct algorithm here
-        
-        query_params = {
-            'decrypt': True,
-        }
-        self.redirect('/success?' + urllib.urlencode(query_params))
+        try:
+            file = self.request.POST.getall('raw_file')
+            name = self.request.get('name')
+            for f in file:
+                sub_file = f.file.read()
+                print("****************")
+                print(sub_file)
+            # add reconstruct algorithm here
+        except:
+            query_params = {
+                'except' : True
+            }
+            self.redirect('/decrypt?' + urllib.urlencode(query_params))
+        else:
+            
+            query_params = {
+                'decrypt': True,
+            }
+            self.redirect('/success?' + urllib.urlencode(query_params))
 
 class Success(webapp2.RequestHandler):
 
