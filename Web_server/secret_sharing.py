2460332ebdec7d22da7fb3b41d5031d8b12dbf92
Neptunennn <guoyuanwu@gatech.edu>
2018-04-27 15:08:35 -0400

    apr27

--- Web_server/secret_sharing.py
+++ Web_server/secret_sharing.py
@@ -28,7 +28,8 @@ from oauth2client import client, tools
 import base64
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from apiclient import errors, discovery
+import apiclient
+#from apiclient import errors, discovery
 import mimetypes
 from email.mime.image import MIMEImage
 from email.mime.audio import MIMEAudio
@@ -47,51 +48,51 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     autoescape=True)
 # [END imports]
 
-def get_credentials():
-    home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join(home_dir, '.credentials')
-    if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,
-                                   'gmail-python-email-send.json')
-    store = oauth2client.file.Storage(credential_path)
-    credentials = store.get()
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-        flow.user_agent = APPLICATION_NAME
-        credentials = tools.run_flow(flow, store)
-        print('Storing credentials to ' + credential_path)
-    return credentials
+# def get_credentials():
+#     home_dir = os.path.expanduser('~')
+#     credential_dir = os.path.join(home_dir, '.credentials')
+#     if not os.path.exists(credential_dir):
+#         os.makedirs(credential_dir)
+#     credential_path = os.path.join(credential_dir,
+#                                    'gmail-python-email-send.json')
+#     store = oauth2client.file.Storage(credential_path)
+#     credentials = store.get()
+#     if not credentials or credentials.invalid:
+#         flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+#         flow.user_agent = APPLICATION_NAME
+#         credentials = tools.run_flow(flow, store)
+#         print('Storing credentials to ' + credential_path)
+#     return credentials
 
-def SendMessage(sender, to, subject, msgHtml, msgPlain, attachmentFile=None):
-    credentials = get_credentials()
-    http = credentials.authorize(httplib2.Http())
-    service = discovery.build('gmail', 'v1', http=http)
-    if attachmentFile:
-        message1 = createMessageWithAttachment(sender, to, subject, msgHtml, msgPlain, attachmentFile)
-    else: 
-        message1 = CreateMessageHtml(sender, to, subject, msgHtml, msgPlain)
-    result = SendMessageInternal(service, "me", message1)
-    return result
+# def SendMessage(sender, to, subject, msgHtml, msgPlain, attachmentFile=None):
+#     credentials = get_credentials()
+#     http = credentials.authorize(httplib2.Http())
+#     service = apiclient.discovery.build('gmail', 'v1', http=http)
+#     if attachmentFile:
+#         message1 = createMessageWithAttachment(sender, to, subject, msgHtml, msgPlain, attachmentFile)
+#     else: 
+#         message1 = CreateMessageHtml(sender, to, subject, msgHtml, msgPlain)
+#     result = SendMessageInternal(service, "me", message1)
+#     return result
 
-def SendMessageInternal(service, user_id, message):
-    try:
-        message = (service.users().messages().send(userId=user_id, body=message).execute())
-        print('Message Id: %s' % message['id'])
-        return message
-    except errors.HttpError as error:
-        print('An error occurred: %s' % error)
-        return "Error"
-    return "OK"
+# def SendMessageInternal(service, user_id, message):
+#     try:
+#         message = (service.users().messages().send(userId=user_id, body=message).execute())
+#         print('Message Id: %s' % message['id'])
+#         return message
+#     except apiclient.errors.HttpError as error:
+#         print('An error occurred: %s' % error)
+#         return "Error"
+#     return "OK"
 
-def CreateMessageHtml(sender, to, subject, msgHtml, msgPlain):
-    msg = MIMEMultipart('alternative')
-    msg['Subject'] = subject
-    msg['From'] = sender
-    msg['To'] = to
-    msg.attach(MIMEText(msgPlain, 'plain'))
-    msg.attach(MIMEText(msgHtml, 'html'))
-    return {'raw': base64.urlsafe_b64encode(msg.as_string())}
+# def CreateMessageHtml(sender, to, subject, msgHtml, msgPlain):
+#     msg = MIMEMultipart('alternative')
+#     msg['Subject'] = subject
+#     msg['From'] = sender
+#     msg['To'] = to
+#     msg.attach(MIMEText(msgPlain, 'plain'))
+#     msg.attach(MIMEText(msgHtml, 'html'))
+#     return {'raw': base64.urlsafe_b64encode(msg.as_string())}
 
 
 
@@ -175,7 +176,7 @@ class Encrypt(webapp2.RequestHandler):
             subject = "subject"
             msgHtml = file
             msgPlain = "Hi\nPlain Email"
-            SendMessage(sender, to, subject, msgHtml, msgPlain)
+            #SendMessage(sender, to, subject, msgHtml, msgPlain)
 
 
 
